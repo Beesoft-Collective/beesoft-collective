234e56d64764fec7223f007a9da142a3dd6cb14e
Salem Olorundare <salemolorundare@gmail.com>
2024-05-28 12:54:43 +0100

    added site icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Beehive software collective",
   description: "We build and manage open source software",
+  icons: {
+    icon: ["/favicon.ico"],
+  },
 };
 
 export default function RootLayout({
